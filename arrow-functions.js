86b3919bf5d85d0d32c58b672fb9c103f5a5f2e8
Hira Khan <phr.hirakhan@gmail.com>
2023-11-04 16:13:40 +0100

    arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,37 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     return a + b;
+// }
+// let sum = addTwoNumbers(3,5);
+// console.log(sum)
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a,b) => {
+    return a + b;
+}
+let sum = addTwoNumbers(5,8);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => (a + b); also valid
+const addTwoNumbers2 = (a, b) => a + b;
+let sum2 = addTwoNumbers(6,3);
+console.log(sum2);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello Beautiful People!');
+
+const sayHello = () => console.log('hello')
+sayHello();
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+    this is a multiline string
+    </p>`
+)
+console.log(returnMultipleLines());
